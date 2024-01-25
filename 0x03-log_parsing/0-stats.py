9d5d7339bc081bd76a4bd859c9262e9b51eedc33
bouhvli <bouhali.hamza.2@gmail.com>
2024-01-25 11:09:54 +0100

    update script

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,8 +6,9 @@ import sys
 
 def print_statistics(ttl_s, status_code_c):
     print('File size: {}'.format(ttl_s))
-    for code in sorted(status_code_c):
-        print('{}: {}'.format(code, status_code_c[code]))
+    for key, value in sorted(status_code_c.items()):
+        if value:
+            print('{}: {}'.format(key, value))
 
 
 def parse_line(line):
@@ -21,29 +22,28 @@ def parse_line(line):
         return None, None, None
 
 
-def main():
-    ttl_size = 0
-    stattus_code_counts = {
-            200: 0, 301: 0, 400: 0,
-            401: 0, 403: 0, 404: 0,
-            405: 0, 500: 0
-            }
-    line_processed = 0
+if __name__ == '__main__':
+    file_size, count = 0, 0
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    status = {key: 0 for key in codes}
+
     try:
         for line in sys.stdin:
-            ip_addr, status_code, file_size = parse_line(line.strip())
-            if ip_addr is not None \
-                    and status_code is not None \
-                    and file_size is not None:
-                ttl_size += file_size
-                stattus_code_counts[status_code] += 1
-                line_processed += 1
-                if line_processed % 10 == 0:
-                    print_statistics(ttl_size, stattus_code_counts)
+            count += 1
+            param = line.split()
+            try:
+                status_code = param[-2]
+                if status_code in status:
+                    status[status_code] += 1
+            except BaseException:
+                pass
+            try:
+                file_size += int(param[-1])
+            except BaseException:
+                pass
+            if count % 10 == 0:
+                print_statistics(file_size, status)
+        print_statistics(file_size, status)
     except KeyboardInterrupt:
-        pass
-    print_statistics(ttl_size, stattus_code_counts)
-
-
-if __name__ == "__main__":
-    main()
+        print_statistics(file_size, status)
+        raise
